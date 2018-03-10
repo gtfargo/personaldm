c417c88d5ed2fc55477afd817783f777bacc670b
George Farro <gtfarro@gmail.com>
2018-03-10 14:18:52 -0600

    adds gatsby-browser

---
+++ gatsby-browser.js
@@ -0,0 +1,16 @@
+exports.onClientEntry = () => {
+  console.log("We've started!")
+}
+
+exports.onInitialClientRender = () => {
+  console.log("ReactDOM.render has executed")
+}
+
+exports.onRouteUpdate = ({ location }) => {
+  console.log('New pathname', location.pathname)
+  if (ga) {
+    ga('send', 'pageview', {
+      page: location.pathname
+    })
+  }
+}
