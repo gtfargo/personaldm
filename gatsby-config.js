ed8cf5d2478f703f68c051d9853703600f43bfac
George Farro <gtfarro@gmail.com>
2018-03-13 11:33:56 -0500

    add more plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,63 +1,76 @@
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`
 });
-const config = require('./src/utils/siteConfig');
+const config = require("./src/utils/siteConfig");
 
 // If the contentfulConfig can't be found assume the site is being built via Netlify with production environment variables
-try { var contentfulConfig = require('./.contentful');}
-catch (e) {
+try {
+  var contentfulConfig = require("./.contentful");
+} catch (e) {
   var contentfulConfig = {
-    "production": {
-      "spaceId": process.env.SPACE_ID,
-      "accessToken": process.env.ACCESS_TOKEN
+    production: {
+      spaceId: process.env.SPACE_ID,
+      accessToken: process.env.ACCESS_TOKEN
     }
-  }
+  };
 }
 
 module.exports = {
+  siteMetadata: {
+    title: `Personal DM`,
+    description: `The best D&D Campaign's you have never played`,
+    siteUrl: `https://personaldm.com`
+  },
   plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-netlify",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-offline",
     {
-    resolve: 'gatsby-plugin-canonical-urls',
-      options: {
-        siteUrl: config.siteUrl,
-      },
+      resolve: `gatsby-plugin-sitemap`
     },
-    'gatsby-plugin-react-helmet',
     {
-    resolve: `gatsby-transformer-remark`,
+      resolve: "gatsby-plugin-canonical-urls",
+      options: {
+        siteUrl: config.siteUrl
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-prismjs`,
-          },
-        ],
-      },
+            resolve: `gatsby-remark-prismjs`
+          }
+        ]
+      }
     },
     {
-    resolve: 'gatsby-source-contentful',
-      options: process.env.NODE_ENV === 'development' ?
-        contentfulConfig.development :
-        contentfulConfig.production
+      resolve: "gatsby-source-contentful",
+      options:
+        process.env.NODE_ENV === "development"
+          ? contentfulConfig.development
+          : contentfulConfig.production
     },
-    'gatsby-plugin-netlify',
-    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: '172388816900731',
+      },
+    },
     {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: {
         id: process.env.GOOGLE_TAGMANAGER_ID,
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-      },
+        includeInDevelopment: false
+      }
     },
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
-        // Setting a color is optional.
         color: `tomato`,
-        // Disable the loading spinner.
-        showSpinner: true,
-      },
-    },
-  ],
-}
+        showSpinner: true
+      }
+    }
+  ]
+};
